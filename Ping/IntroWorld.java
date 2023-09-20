50ba166a9e87fcadaa23ad49100eb35f80b0f2ef
Gergö Kadar <kadargergo20@gmail.com>
2023-09-20 15:59:47 +0200

    pizza er rent faktisk false hver gang man trykker restart

--- Ping/IntroWorld.java
+++ Ping/IntroWorld.java
@@ -4,7 +4,7 @@ public class IntroWorld extends World {
     public IntroWorld() { 
         // Sætter introWorlds dimensioner (højde, længde, og cell size)
         super(500, 700, 1); 
-        
+        PingWorld.isPizzaTurnedOn = false;
         // Tilføjer StartButton til introWorld. 
         addObject(new StartButton(), getWidth() / 2, getHeight() / 2);
         addObject(new PizzaButton(), getWidth() / 2, 500);
